2b3c6fcfe275b612fdfa7858fc62c6f368f20349
hosmara <hoschavezc@gmail.com>
2019-11-07 21:27:06 -0600

    ControladorJos

---
+++ P_Dinadeco/Controllers/CambiarEstadoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P_Dinadeco.Controllers
+{
+    public class CambiarEstadoController : Controller
+    {
+        // GET: CambiarEstado
+        public ActionResult CamEstado()
+        {
+            return View();
+        }
+    }
+}
---
+++ P_Dinadeco/Controllers/TramitarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P_Dinadeco.Controllers
+{
+    public class TramitarController : Controller
+    {
+        // GET: Tramitar
+        public ActionResult Tramitar()
+        {
+            return View();
+        }
+    }
+}
